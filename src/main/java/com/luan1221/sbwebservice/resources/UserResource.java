116141f383424cc6df7603f9e827e3d0ec54f4e7
luan1221 <lssa@cin.ufpe.br>
2022-05-02 18:57:16 -0300

    service layer

--- src/main/java/com/luan1221/sbwebservice/resources/UserResource.java
+++ src/main/java/com/luan1221/sbwebservice/resources/UserResource.java
@@ -1,17 +1,30 @@
 package com.luan1221.sbwebservice.resources;
 
 import com.luan1221.sbwebservice.entities.User;
+import com.luan1221.sbwebservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "1234", "9873");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
---
+++ src/main/java/com/luan1221/sbwebservice/services/UserService.java
@@ -0,0 +1,26 @@
+package com.luan1221.sbwebservice.services;
+
+import com.luan1221.sbwebservice.entities.User;
+import com.luan1221.sbwebservice.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> optional = repository.findById(id);
+        return optional.get();
+    }
+
+}
