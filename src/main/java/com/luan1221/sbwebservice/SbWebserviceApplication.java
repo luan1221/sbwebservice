2a3f188d9ef73a4fca4c838fa0b3083efd63f4a1
luan1221 <lssa@cin.ufpe.br>
2022-05-02 17:55:44 -0300

    initial commit

---
+++ src/main/java/com/luan1221/sbwebservice/SbWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.luan1221.sbwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luan1221/sbwebservice/SbWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan1221.sbwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
