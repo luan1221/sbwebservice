106f763284aad9d84124a1486222e915c0f81ada
luan1221 <lssa@cin.ufpe.br>
2022-05-02 18:27:25 -0300

    jpa configurations

--- src/main/java/com/luan1221/sbwebservice/entities/User.java
+++ src/main/java/com/luan1221/sbwebservice/entities/User.java
@@ -2,6 +2,11 @@ package com.luan1221.sbwebservice.entities;
 
 import lombok.*;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -9,8 +14,11 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
