d96c3c62899e8505a334e07c623a6ce197b97950
luan1221 <lssa@cin.ufpe.br>
2022-05-05 21:15:40 -0300

    user insert

--- src/main/java/com/luan1221/sbwebservice/resources/UserResource.java
+++ src/main/java/com/luan1221/sbwebservice/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.luan1221.sbwebservice.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,6 +27,14 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = service.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
--- src/main/java/com/luan1221/sbwebservice/services/UserService.java
+++ src/main/java/com/luan1221/sbwebservice/services/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
         return optional.get();
     }
 
+    public User insert(User user) {
+        return repository.save(user);
+    }
+
 }
