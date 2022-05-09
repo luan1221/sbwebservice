cdce2d41b8ac8408733ec113fa430dbd0ac7d5f6
luan1221 <lssa@cin.ufpe.br>
2022-05-09 11:49:36 -0300

    delete exception handling

--- src/main/java/com/luan1221/sbwebservice/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/luan1221/sbwebservice/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.luan1221.sbwebservice.resources.exceptions;
 
+import com.luan1221.sbwebservice.services.exceptions.DatabaseException;
 import com.luan1221.sbwebservice.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,4 +20,14 @@ public class ResourceExceptionHandler {
         StandardError se = new StandardError(Instant.now(), status.value(), error, err.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(se);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException err, HttpServletRequest request) {
+        String error = "Database error.";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError se = new StandardError(Instant.now(), status.value(), error, err.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(se);
+    }
+
+
 }
---
+++ src/main/java/com/luan1221/sbwebservice/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.luan1221.sbwebservice.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
